de25d5a4abd04ed7cb7da7c24e203c9a5a6f55c5
Brian Jesse <brian.jesse@gmail.com>
2017-11-17 04:35:23 -0600

    feat(webpack): caching in standard directory

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 'use strict';
 
+let crypto = require('crypto');
+let fs = require('fs');
+let findCacheDir = require('find-cache-dir');
 let virtualenv = require('virtualenv');
 
-const package = require.resolve('./package');
-const env = virtualenv(package);
+const cache = findCacheDir({
+  name: 'javascripthon-loader',
+  thunk: true,
+  create: true,
+});
+const env = virtualenv(require.resolve('./package'));
 
 module.exports = function (content) {
   let cb = this.async();
-  let pj = env.spawn('pj', ['--inline-map', '-s', content]);
-  let out = '';
-  pj.stdout.on('data', data => {
-    out += data;
-  });
+  let hash = crypto
+    .createHash(this.options.output.hashFunction)
+    .update(content)
+    .digest(this.options.output.hashDigest)
+    .substring(0, this.options.output.hashDigestLength);
+  let cacheFile = cache(`${hash}.js`);
+
+  let close = (err, data) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    if (this.sourceMap) {
+      fs.readFile(`${cacheFile}.map`, 'utf8', (e, map) => cb(e, data, JSON.parse(map)));
+      return;
+    }
+    cb(null, data);
+  };
+
+  let read = (err, code) => {
+    if (err || code) {
+      cb(err || `javascripthon-loader exited with error status: ${code}`);
+      return;
+    }
+    fs.readFile(cacheFile, 'utf8', close);
+  };
+
+  if (fs.existsSync(cacheFile)) {
+    read();
+    return;
+  }
+
+  let pj = env.spawn('pj', [this.resourcePath, '-o', cacheFile]);
   let err = '';
   pj.stderr.on('data', data => {
     err += data;
   });
-  pj.on('close', code => {
-    let sourcemap;
-    let i = out.lastIndexOf('//#')
-    if (~i) {
-      let line = out.substring(sourcemap);
-      let encoded = line.substring(line.indexOf(',') + 1);
-      sourcemap = JSON.parse(Buffer.from(encoded, 'base64'));
-    }
-    if (code === 0) {
-      cb(null, out, sourcemap);
-    } else {
-      console.error(`javascripthon-loader exited with error status: ${code}`);
-      if (err) {
-        console.error(err);
-      }
-    }
-  });
+  pj.on('close', code => read(err || null, code));
 };
 
